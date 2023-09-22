5f78b341e3a22508b48cea2f8cfaf92fb3a42cf3
Jose Xavier Fuentes <josexfuentes27@gmail.com>
2023-09-22 16:18:54 -0700

    moving scene scripts to inner folder

--- Assets/Scripts/SceneScripts/TwoPlayerGame.cs
+++ Assets/Scripts/SceneScripts/TwoPlayerGame.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Play : MonoBehaviour
+public class TwoPlayerGame : MonoBehaviour
 {
     public void LoadGameScene()
     {
