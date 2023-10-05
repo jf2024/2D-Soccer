af74216055235079363ef3b581b8e199d7a48b59
Jose Xavier Fuentes <josexfuentes27@gmail.com>
2023-10-04 19:22:06 -0700

    improved post code and handled edge case

--- Assets/Scripts/Post.cs
+++ Assets/Scripts/Post.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 
 public class Post : MonoBehaviour
 {
-    public float slowDownFactor = 2.0f;     //dead ball power up can be 0.2
+    public float slowDownFactor = 0.8f;     //dead ball power up can be 0.2
+    public float deadBall = 0.1f;
+    public float push = 1.5f;
+
+    private Rigidbody2D collidedBall = null;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ball"))
         {
             Rigidbody2D ballRigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
+            collidedBall = ballRigidbody;
             ballRigidbody.velocity *= slowDownFactor;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (collidedBall != null)
+        {
+            // check if the ball's velocity below the deadBall 
+            if (collidedBall.velocity.magnitude < deadBall)
+            {
+                // apply force to move the ball away from the post
+                Vector2 forceDirection = (transform.position - collidedBall.transform.position).normalized;
+                collidedBall.AddForce(forceDirection * push, ForceMode2D.Impulse);
+            }
         }
     }
 }
